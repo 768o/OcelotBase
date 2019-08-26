0d0c66a25632b535fa786e1636ce0b6224157b3d
Pliterture <1051209415@qq.com>
2019-08-26 10:46:06 +0800

    first

---
+++ Api/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseUrls("http://127.0.0.1:8683")
+            .ConfigureAppConfiguration(conf => {
+                conf.AddJsonFile("ocelotsettings.json", optional: false, reloadOnChange: true);
+            });
+    }
+}
